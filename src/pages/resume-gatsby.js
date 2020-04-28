2d84210ea36a6b144f02d549027e20c8bc99adda
arhoy <you@example.com>
2020-04-27 21:28:50 -0600

    adfasdf

--- src/pages/resume-gatsby.js
+++ src/pages/resume-gatsby.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { graphql } from 'gatsby';
-import styled from '@emotion/styled';
-import { PDFDownloadLink } from '@react-pdf/renderer';
+// import { graphql } from 'gatsby';
+// import styled from '@emotion/styled';
+// import { PDFDownloadLink } from '@react-pdf/renderer';
 
 import { Layout } from '../layouts/Layout';
 
-import { MyDocument } from '../components/MyDocument';
-import { Resume } from '../components/_resume/Resume';
+// import { MyDocument } from '../components/MyDocument';
+// import { Resume } from '../components/_resume/Resume';
 
 const ResumePage = () => {
-  return (
-    <Layout>
-      {/* <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
-        {({ blob, url, loading, error }) =>
-          loading ? 'Loading document...' : 'Download now!'
-        }
-      </PDFDownloadLink>
-      <MyDocument /> */}
-      NEW
-    </Layout>
-  );
+  return <Layout>NEW</Layout>;
 };
 
 export default ResumePage;
